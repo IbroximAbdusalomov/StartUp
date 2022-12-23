9fc6ae84b21699c5003d7c211c379ad172ee1118
ibroxim <ibrohimabdusalomov395@gmail.com>
2022-12-23 21:40:46 +0500

    test search

--- app/search.py
+++ app/search.py
@@ -1,12 +1,12 @@
 from aiogram.dispatcher import FSMContext
 
-from buttons import main_mane_2_uz, main_mane_2_en
+from buttons import main_mane_2_uz, main_mane_2_en, sub_category_uz, sub_category_en
 from create_bot import db, Dispatcher, bot
 from aiogram import types
 
 from messages import Tanlang_uz, Tanlang_en
 from states import CategorysForSearch
-from app.func_ import check_lan_and_btn
+from func_ import check_lan_and_btn
 
 
 async def start_category(callback: types.CallbackQuery):
@@ -17,6 +17,7 @@ async def start_category(callback: types.CallbackQuery):
 async def category_1(callback: types.CallbackQuery, state: FSMContext):
     async with state.proxy() as data:
         data['product_type'] = callback.data
+        await check_lan_and_btn(callback.from_user.id, Tanlang_uz, Tanlang_en, sub_category_uz(), sub_category_en())
     await CategorysForSearch.next()
 
 
@@ -27,7 +28,7 @@ async def category_2(callback: types.CallbackQuery, state: FSMContext):
         post = db.search_posts(category_1=data.get('product_type'), category_2=data.get('sub_category'))
         if post:
             for i in post:
-                await bot.send_photo(callback.from_user.id, text=f"""{i[2]}\n{i[7]}\n{i[5]}\n{i[6]}\n""")
+                await bot.send_photo(callback.from_user.id, text=f"""{i[2]}\n{i[7]}\n{i[5]}\n{i[6]}""")
 
 
 def register_search(dp: Dispatcher):
