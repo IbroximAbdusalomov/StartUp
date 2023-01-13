1cdb5dd2869bb77c321ce9f13410f6398320fbc6
IbroximAbdusalomov <122609470+IbroximAbdusalomov@users.noreply.github.com>
2023-01-13 16:12:46 +0500

    Update create_bot.py

--- create_bot.py
+++ create_bot.py
@@ -5,7 +5,7 @@ from aiogram import Bot, Dispatcher
 
 from database import DataBase
 logging.basicConfig(level=logging.INFO)
-API_TOKEN = '5663518655:AAHTdZh4HaQRfRTKaykpFDkYhNy6YajLUN4'
+API_TOKEN = 'TOKEN'
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot, storage=MemoryStorage())
 db = DataBase('database.sqlite')
